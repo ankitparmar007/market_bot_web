e38459c69f6b139ce59aa126d60c46e3e994b4bf
Ankit Parmar <ankitparmar0070@gmail.com>
2025-09-20 18:46:51 +0530

    refactor: opition oi chart view

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "916ed07fe1a65197fadd22af7851afbd
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b472e00c749022c701d9d2bb1d3ec5c8",
+"flutter_bootstrap.js": "8a2fdcaaba0ef031440787f4e2f5e899",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "102f803e203275a8c9955ff533e13654",
 "/": "102f803e203275a8c9955ff533e13654",
-"main.dart.js": "da91f382af140b56d50de82016ce6a4e",
+"main.dart.js": "d7ead83842dd2365d6f0ae2253980ba8",
 "manifest.json": "d06ae40fae21af499d0c1d3c1d64d770",
 "version.json": "c4f892dd43eda3e4b108a356d4cd1490"};
 // The application shell files that are downloaded before a service worker can
