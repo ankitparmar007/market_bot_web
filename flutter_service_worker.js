86425a41fa19fb6d26b06d86d9b9060337c5cdec
Ankit Parmar <ankitparmar0070@gmail.com>
2025-11-18 18:41:36 +0530

    Bull & Bear Icon Added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+const RESOURCES = {"assets/AssetManifest.bin": "9bcc29114e90ca068e4785a947967243",
+"assets/AssetManifest.bin.json": "25c8262675d0c0fd1408ed34406289d7",
+"assets/AssetManifest.json": "56de412840a2878bb621fdb56d1afa90",
+"assets/assets/svg/bear.svg": "64b229d0d9cc31b8f5f2f64686224a7d",
+"assets/assets/svg/bull.svg": "57286e82f0e20701071ca019ce2247ca",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "252eba520bbdb7b6b67f0564b6169e5e",
 "assets/NOTICES": "c879152c33fd62d24f4a643489f920de",
@@ -25,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "00eef1276c7839edaa7350612fd992f9",
+"flutter_bootstrap.js": "74634ae68a93984fb06dfad0194caf32",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e4975a8daa37fac88ec323176473fadb",
 "/": "e4975a8daa37fac88ec323176473fadb",
-"main.dart.js": "3d6d8e84fb1cb03e8c12ad7b90ab0f00",
+"main.dart.js": "53976cdc133b74db03d696840d2ee184",
 "manifest.json": "784035a0a2c5a60dfcbf722353a95862",
 "version.json": "338231507dcf95e313dbca4d79d866c0"};
 // The application shell files that are downloaded before a service worker can
