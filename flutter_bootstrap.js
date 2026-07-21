d7cf1ba5f0119135d874a0146bd249e847141910
Ankit Parmar <73648141+ankitparmar007@users.noreply.github.com>
2026-07-21 20:58:37 +0530

    feat: Reimplemented OptionOiLineMarkers for OHLC and Pivot with Callout Marker for enhanced visualization, indices view now support adx and tsi and updated app version to 1.37

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2153023002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3112795489" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
